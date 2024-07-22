61970723b68a5e2c800eac8838f16aea1d2cf57d
hermani <hermani457@gmail.com>
2024-07-22 13:35:32 -0400

    chore: Update PDF route to use promises for file system operations

--- src/app/api/pdf/route.js
+++ src/app/api/pdf/route.js
@@ -5,31 +5,49 @@ import { compile } from "@fileforge/react-print";
 import Template from "../../components/Template";
 import fs from "fs";
 import { promises as fss } from 'fs';
+import { Resend } from "resend";
 // const path = require("path");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const ff = new FileforgeClient({
   apiKey: process.env.ONEDOC_API_KEY,
 });
 
-export const GET = async () => {
-  const HTML = await compile(Template());
-  // const file = await fss.readFile(process.cwd() + '/public/sakura.png');
+export const POST = async (req) => {
+  const body = await req.json();
+  const { name, guest, email } = body;
+  console.log(body);
+  const HTML = await compile(<Template name={name} guest={guest} email={email}/>);
   const imagePath = await fss.readFile(process.cwd() + '/public/Picture1.png');
-  // const imageBuffer = fs.readFileSync(imagePath);
   const imagePath2 = await fss.readFile(process.cwd() + '/public/Picture2.png');
-  // const imageBuffer2 = fs.readFileSync(imagePath2);
+  const imagePath3 = await fss.readFile(process.cwd() + '/public/border1.png');
+  const imagePath4 = await fss.readFile(process.cwd() + '/public/border.png');
+  const imagePath5 = await fss.readFile(process.cwd() + '/public/2.png');
 
   const pdfStream = await ff.pdf.generate(
     [
       new File([HTML], "index.html", {
         type: "text/html",
       }),
-      new File([imagePath], "Picture1.png", {
-        type: "image/png",  
-      }),
-      new File([imagePath2], "Picture2.png", {
+      // new File([imagePath], "Picture1.png", {
+      //   type: "image/png",  
+      // }),
+      // new File([imagePath2], "Picture2.png", {
+      //   type: "image/png",
+      // }),
+      new File([imagePath4], "border.png", {
         type: "image/png",
       }),
+      // new File([imagePath5], "2.png", {
+      //   type: "image/png",
+      // }),
+      // new File([imagePath3], "border1.png", {
+      //   type: "image/png",
+      // }),
+      // new File([imagePath3], "border1.png", {
+      //   type: "image/png",
+      // }),
     ],
     {
       options: {
@@ -42,7 +60,7 @@ export const GET = async () => {
     }
   );
 
-  pdfStream.pipe(fs.createWriteStream("./result3.pdf"));
+  // pdfStream.pipe(fs.createWriteStream("./result3.pdf"));
 
   const chunks = [];
 
@@ -51,6 +69,25 @@ export const GET = async () => {
   }
 
   const pdfBuffer = Buffer.concat(chunks);
+
+  const { data, error } = await resend.emails.send({
+    from: "Yme.cL <send@yme.cl>",
+    to: email,
+    subject: 'Hello World',
+    html: '<strong>It works!</strong>',
+    attachments: [
+      {
+        filename: 'invitation.pdf',
+        content: pdfBuffer.toString('base64'),
+        encoding: 'base64',
+        contentType: 'application/pdf',
+      },
+    ],
+  });
+
+  if (error) {
+    return console.error({ error });
+  }
 
   return new Response(pdfBuffer, {
     headers: {
