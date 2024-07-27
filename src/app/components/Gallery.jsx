3bd96225c20f6cdab640b54eca7c57ec41449b78
hermani <hermani457@gmail.com>
2024-07-27 19:55:52 -0400

    feat: Add embla-carousel-react dependency and implement carousel component
    This commit adds the embla-carousel-react dependency to the package.json file and implements a carousel component in the src/app/components/ui/carousel.jsx file. The carousel component uses the embla-carousel-react library to create a customizable carousel with previous and next navigation buttons. This change enhances the user experience by allowing the display of multiple items in a sliding carousel format.

---
+++ src/app/components/Gallery.jsx
@@ -0,0 +1,13 @@
+import { Carousel } from "./ui/carousel";
+
+export default function Gallery() {
+  return (
+    <Carousel>
+      <img src="/gallery/1.jpg" alt="Gallery 1" />
+      <img src="/gallery/2.jpg" alt="Gallery 2" />
+      <img src="/gallery/3.jpg" alt="Gallery 3" />
+      <img src="/gallery/4.jpg" alt="Gallery 4" />
+      <img src="/gallery/5.jpg" alt="Gallery 5" />
+    </Carousel>
+  );
+}
