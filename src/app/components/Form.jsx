217f72805b519bfc4bf77f0baf7c2b6aa2191af8
hermani <hermani457@gmail.com>
2024-07-30 17:52:59 -0400

    feat: Add placeholder for address field in Form component

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -112,6 +112,7 @@ const Form = () => {
             id="companion"
             onChange={(e) => setAddress(e.target.value)}
             value={address}
+            placeholder="456 Main St, City, Country"
             className="bg-gray-50 border border-gray-300 text-unolight text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
           <span className="text-sm">
