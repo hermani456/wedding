005a57f5b1c6673ddc8e0d81d4510c42cfdb2761
hermani <hermani457@gmail.com>
2024-09-02 23:18:11 -0400

    hola

--- test.js
+++ test.js
@@ -1,35 +1,9 @@
-// const invitationCodes = [
-//   "1234",
-//   "5678",
-//   "91011",
-//   "121314",
-//   "151617",
-//   "181920",
-//   "212223",
-//   "242526",
-// ];
+const test = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-// const invitationCode = "1234a";
-
-// if (!invitationCodes.includes(invitationCode)) {
-//   console.log("Invalid invitation code");
-// }
-
-// for (let i = 0; i < 50; i++) {
-//   const shortId = crypto.randomUUID().split("-").join("").substring(0, 16);
-//   console.log(`INSERT INTO invitationCodes (code) VALUES('${shortId}');`);
-// }
-
-// img_1527
-// img_1065
-// img_1871
-// img_5330
-// img_7624
-// img_6659
-// img_6045
-
-// 4
-// 12
-// 3
-
-// cambiar nombre de invitados en pdf 
+export default test
